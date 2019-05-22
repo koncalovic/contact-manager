2fa0209d5eb730cfd35908d2b40adb0b375d1668
koncalovic <milanakoncalovic@hotmail.com>
2019-05-22 13:23:34 +0200

    delete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Contacts from "./components/Contacts";
 import Header from "./components/Header";
 import Container from "react-bootstrap/Container";
+import { Provider } from "./context";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -9,12 +10,14 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header branding="Contact Manager" />
-        <Container>
-          <Contacts />
-        </Container>
-      </div>
+      <Provider>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <Container>
+            <Contacts />
+          </Container>
+        </div>
+      </Provider>
     );
   }
 }
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,39 +1,22 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@gmail.com",
-        phone: "555-555-555"
-      },
-      {
-        id: 2,
-        name: "Karen Williams",
-        email: "karen@gmail.com",
-        phone: "222-222-222"
-      },
-      {
-        id: 3,
-        name: "Henry Johnson",
-        email: "henry@gmail.com",
-        phone: "111-111-111"
-      }
-    ]
-  };
-
   render() {
-    const { contacts } = this.state;
-
     return (
-      <div>
-        {contacts.map(contact => (
-          <Contact key={contact.id} contact={contact} />
-        ))}
-      </div>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
